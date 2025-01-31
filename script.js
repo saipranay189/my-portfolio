a21c916cf91c873c3b79a6ac86219fac9fcb91c9
saipranay189 <BADVEETISAIPRANAY06@GMAIL.COM>
2025-01-31 10:34:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
+gsap.from(".hero-image", { duration: 1.2, x: 100, opacity: 0, ease: "power3.out" });
